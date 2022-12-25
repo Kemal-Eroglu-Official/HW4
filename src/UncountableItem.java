579d260161b8139ae565f2da1d340c37acc23e7d
Kemal Eroğlu <82586326+Kemal-Eroglu-Official@users.noreply.github.com>
2022-12-25 13:05:38 +0000

    Delete UncountableItem.java

--- src/UncountableItem.java
+++
@@ -1,9 +0,0 @@
-public class UncountableItem extends Item {
-	
-	private int itemMass;
-
-	public UncountableItem(String itemCode, int volume, String itemSerialNumber, int itemMass) {
-		super(itemCode, volume, itemSerialNumber);
-		this.itemMass = itemMass;
-	}
-}
