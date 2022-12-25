6babfafe95de86cf18bee2624a6baf3e1b7ad260
Kemal Eroğlu <82586326+Kemal-Eroglu-Official@users.noreply.github.com>
2022-12-25 13:18:00 +0000

    Add files via upload

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -6,9 +6,32 @@ public class Command {
         NewObject, // 1
         Load, // 2
         Ship, // 3
-        Show // 4
+        Show; // 4
+
+        public CommandType valueOfInt(int n) {
+            switch (n) {
+                case 1: return NewObject;
+                case 2: return Load;
+                case 3: return Ship;
+                case 4: return Show;
+                default: return null;
+            }
+        }
     }
 
     private CommandType type;
-    private HashMap<String, Double> attributes;
+    private HashMap<String, String> attributes;
+
+    Command(CommandType type, HashMap<String, String> attrubutes) {
+        this.type = type;
+        this.attributes = attrubutes;
+    }
+
+    public HashMap<String, String> getAttributes() {
+        return new HashMap<>(this.attributes);
+    }
+
+    public CommandType getType() {
+        return type;
+    }
 }
