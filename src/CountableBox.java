215db30de72a2adacca397a72f44fbe7a1aed78a
Kemal Eroğlu <82586326+Kemal-Eroglu-Official@users.noreply.github.com>
2022-12-25 13:05:09 +0000

    Delete CountableBox.java

--- src/CountableBox.java
+++
@@ -1,8 +0,0 @@
-public class CountableBox extends Box implements Countable {
-	private int itemCount;
-	
-	public CountableBox(String boxCode, int volume, int boxSerialNumber, int itemCount) {
-		super(boxCode, volume, boxSerialNumber);
-		this.itemCount = itemCount;
-	}
-}
