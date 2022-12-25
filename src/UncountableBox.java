e27576f51acf9c26c6fcf7594ec27658d9146257
Kemal Eroğlu <82586326+Kemal-Eroglu-Official@users.noreply.github.com>
2022-12-25 13:05:32 +0000

    Delete UncountableBox.java

--- src/UncountableBox.java
+++
@@ -1,8 +0,0 @@
-public class UncountableBox extends Box{
-	private int itemMass;
-
-	public UncountableBox(String boxCode, int volume, int boxSerialNumber, int itemMass) {
-		super(boxCode, volume, boxSerialNumber);
-		this.itemMass = itemMass;
-	}
-}
